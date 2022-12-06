20d873e01835d1c0d1e57b86f4258e7432bf982f
Gleb Golovnev <glebgolovnev2004@gmail.com>
2022-12-06 13:17:57 +0300

    Created OrderPayments for BelSystem and Foreign System

---
+++ OnlineShop/OrderPayments/ForeignSystemOrderPayment.cs
@@ -0,0 +1,21 @@
+﻿using Contracts.Classes;
+
+namespace OnlineShop.OrderPayments
+{
+    public class ForeignSystemOrderPayment : OrderPaymentMethod
+    {
+        public ForeignSystemOrderPayment(User user) : base(user)
+        {
+        }
+
+        protected override void BonusAccrual()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override decimal DiscountCalculation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
